f3908f6454e7555fc569e8be5112cc49ee5d491f
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-03-09 11:02:41 -0600

    add() tests

---
+++ js/add.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports.add = (a, b) => {
+    return (a+b);
+};
--- test/test.js
+++ test/test.js
@@ -7,4 +7,10 @@ describe('add()', () => {
     it('should be a function', () => {
         assert.isFunction(add);
     });
+    it('should return a number', () => {
+        assert.isNumber(add(1, 2));
+    });
+    it('should work with negative numbers', () => {
+        assert.equal(add(1, -2), -1);
+    });
 });
